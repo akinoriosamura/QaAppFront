23c0a91a806deed18d8cc9fbeea3274841462a6f
akinoriosamura <osamura.akinori@gmail.com>
2017-12-24 17:31:11 +0900

    delete question

--- src/pages/Spe_QueContent.vue
+++ src/pages/Spe_QueContent.vue
@@ -3,8 +3,10 @@
     <custom-toolbar v-bind="toolbarInfo" @setId-event="setUserId" @logout-event="redirectHome"></custom-toolbar>
         <textarea class="question" v-model="content" placeholder="ここに質問を記入してください。"></textarea>
 
-        <v-ons-button v-if="type=='create'" class="stripe-button" modifier="large" style="margin: 0px 0" @click="setContent(user_id, price, content, specialist_id)">{{ price }} 円で質問</v-ons-button>
-        <v-ons-button v-else-if="type=='edit'" class="stripe-button" modifier="large" style="margin: 0px 0" @click="editContent(post_id, content)">編集</v-ons-button>
+        <v-ons-button v-if="type=='create'" modifier="large" style="margin: 0px 0" @click="setContent(user_id, price, content, specialist_id)">{{ price }} 円で質問</v-ons-button>
+
+        <v-ons-button v-else-if="type=='edit'" modifier="large" style="margin: 0px 0" @click="editContent(post_id, content)">編集</v-ons-button>
+        <button v-if="type=='edit'" style="margin: 0px 0" @click="deleteContent(post_id)">削除</button>
 
   </v-ons-page>
 </template>
@@ -45,6 +47,10 @@ export default {
           console.log('body:', response.data)
           alert("質問しました。MyQ&Aから編集できます。")
         })
+        .catch( (response) => {
+          console.error('error:', response);
+          alert("質問に失敗しました。")
+        });
         // pop navigator stack and back to previous page
         this.$store.commit('navigator/pop')
       }
@@ -68,10 +74,35 @@ export default {
           console.log('body:', response.data)
           alert("編集しました。")
         })
+        .catch( (response) => {
+          console.error('error:', response);
+          alert("編集に失敗しました。")
+        });
         // pop navigator stack and back to previous page
         this.$store.commit('navigator/reset')
         this.$store.commit('tabbar/set', 2)
       }
+    },
+    deleteContent(post_id) {
+      axios.delete(process.env.API_DOMAIN_URL + "v1/posts/" + post_id, {
+        headers: {
+          'access-token': VueCookie.get('access-token'),
+          'client': VueCookie.get('client'),
+          'uid': VueCookie.get('uid'),
+          'content-type': 'application/json'
+        }
+      })
+      .then(response => {
+        console.log('body:', response.data)
+        alert("削除しました。")
+      })
+      .catch( (response) => {
+        console.error('error:', response);
+        alert("削除に失敗しました。")
+      });
+      // pop navigator stack and back to previous page
+      this.$store.commit('navigator/reset')
+      this.$store.commit('tabbar/set', 2)
     },
     // get login user id from CustomToolbar
     setUserId(user_id) {
